9c52ddb9d2fe574223b951eb0f0f1073adc22e4f
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2020-11-20 12:45:10 +0000

    layers and styling config

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,5 +1,5 @@
-import { developablesitesPopup, permissionedPopup, notassessedPopup, tclaPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
-import { developablesitesStyle, tcwmdcStyle, greenbeltStyle, floodzonesStyle, allsubmittedStyle, permissionedStyle, notassessedStyle, tclaStyle} from './Styles'
+import { streetlightingPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
+import { streetlightingStyle} from './Styles'
 
 const Configuration = {
     Map: {
@@ -27,98 +27,21 @@ const Configuration = {
         //},
 
         {
-            key: 'Green Belt',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:green_belt_os&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Street Lights',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highway_assets:street_light_reporting&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
             layerOptions: {
                 maxZoom: 2,
-                style: greenbeltStyle
+                style: streetlightingStyle,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker (latlng)
+
+                }
+
             },
-            displayOverlay: true,
+            displayOverlay: false,
             visibleByDefault: true
         },
         
-        {
-            key: 'Flood Risk Zones',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=flooding:flood_zones&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: floodzonesStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Town Centre Living Area',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_tcla&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: tclaPopup,    
-                maxZoom: 2,
-                style: tclaStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },       
-        
-        {
-            key: 'Town Centre West Mayoral Development Corporation Area',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:town_centre_west_mdc&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: tcwmdcStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Sites with plannning permission for housing',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:schema2020_resi_permissioned&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: permissionedPopup,
-                maxZoom: 2,
-                style: permissionedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'All sites considered in SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_allsites&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                maxZoom: 2,
-                style: allsubmittedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: false
-        },
-
-        {
-            key: 'Developable sites SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_sites&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: developablesitesPopup,
-                maxZoom: 2,
-                style: developablesitesStyle
-
-            },
-            displayOverlay: true,
-            visibleByDefault: true
-        },
-
-        {
-            key: 'Sites that did not pass first sift of SHLAA 2020',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:shlaa2020_not_assessed&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: notassessedPopup,
-                maxZoom: 2,
-                style: notassessedStyle
-            },
-            displayOverlay: true,
-            visibleByDefault: false
-        },
-
         {
             key: 'os1250_line',
             url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
--- src/Styles/index.js
+++ src/Styles/index.js
@@ -1,75 +1,21 @@
-const allsubmittedStyle = {
-    color: '#003c30',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#01665e',
-    fillOpacity: 0
-}
-const tclaStyle = {
-    color: '#49006a',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#7a0177',
-    fillOpacity: 0.25
-}
-const tcwmdcStyle = {
-    color: '#fdb462',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#fdb462',
-    fillOpacity: 0
-}
-const developablesitesStyle = { //This needs changing based on how the input of deliverable and non developable sites works out.
-    color: '#bc80bd',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#fccde5',
-    fillOpacity: 0.5
-}
-function getColor_floodzones(d) {
-switch  (d) {   case 'Zone2'    :
-                    return '#8dd3c7'    
-                case 'Zone3'    :
-                    return '#80b1d3'    
+function getColor_streetlights(d) {
+switch  (d) {   case '1'    :
+                    return '#33a02c'    
+                case '2'    :
+                    return '#ff7f00'
+                case '3'    :
+                    return '#e31a1c'    
             }
 }
-function floodzonesStyle (feature) {
+function streetlightsStyle (feature) {
 return {
-    color: getColor_floodzones (feature.properties.type),
+    color: getColor_streetlights (feature.properties.raise_new_job),
     weight: 1,
     opacity: 1,
-    fillColor: getColor_floodzones (feature.properties.type),
+    fillColor: getColor_streetlights (feature.properties.raise_new_job),
     fillOpacity: 0.5
     }
 }
-const greenbeltStyle = {
-    color: '#b3de69',
-    weight: 2,
-    opacity: 0.25,
-    fillColor: '#b3de69',
-    fillOpacity: 0.25
-}
-const permissionedStyle = {
-    color: '#e31a1c',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#fb9a99',
-    fillOpacity: 0.5
-}
-const notassessedStyle = {
-    color: '#081d58',
-    weight: 2,
-    opacity: 1,
-    fillColor: '#253494',
-    fillOpacity: 0.5
-}
 export {
-allsubmittedStyle,
-tcwmdcStyle,
-developablesitesStyle,
-floodzonesStyle,
-greenbeltStyle,
-permissionedStyle,
-notassessedStyle,
-tclaStyle
+streetlightingStyle
 }
