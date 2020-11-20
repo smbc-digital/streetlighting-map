3a3c93028058182ebe639e25ae84d03b30b05dc0
Dan Kinder <d.kinder02@gmail.com>
2020-11-20 16:22:58 +0000

    styling changes

--- src/Styles/index.js
+++ src/Styles/index.js
@@ -1,16 +1,16 @@
 function getColor_streetlights(d) {
-switch  (d) {   case '1'    :
+switch  (d) {   case "1   "    :
                     return '#33a02c'    
-                case '2'    :
+                case "2   "    :
                     return '#ff7f00'
-                case '3'    :
+                case "3   "    :
                     return '#e31a1c'    
             }
 }
 function streetlightingStyle (feature) {
 return {
     radius: 6,
-    color: getColor_streetlights (feature.properties.raise_new_job),
+    color: '#000',//getColor_streetlights (feature.properties.raise_new_job),
     weight: 2,
     opacity: 1,
     fillColor: getColor_streetlights (feature.properties.raise_new_job),
