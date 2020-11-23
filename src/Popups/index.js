28445cbcacd058efae694a7ab1cc8259d4183d6a
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2020-11-23 14:38:01 +0000

    configure popup

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,75 +1,22 @@
-const developablesitesPopup = (feature, layer) => {
+const streetLightingPopup = (feature, layer) => {
   
-  const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Developable sites SHLAA 2020</p>
+  const content = `<div class="item"><i class="fa fa-lightbulb-o" aria-hidden="true"></i><p class="title">Street Light</p>
   <p></p>
-  <p class="info">Site Address: ${feature.properties.site_address}</p>
-  <p class="info">Site Description: ${feature.properties.site_description_2020}</p>
-  <p class="info">Site Area (Ha): ${feature.properties.site_area}</p>
-  <p class="info">Site Capacity: ${feature.properties.capacity}</p>
-  <p class="info">Delivery Years: ${feature.properties.delivery_years}</p>
+  <p class="info">Location: ${feature.properties.location_description}</p>
+  <p class="info">Number on Lamp Post: ${feature.properties.feature_id}</p>
+  <p class="info">CAID: ${feature.properties.central_asset_id}</p>
+  <p class="info">Site Code: ${feature.properties.site_code}</p>
+  <p class="info">Job Status: ${feature.properties.job_status}</p>
+  <p class="info">Fault Reported: ${feature.properties.job_entry_date}</p>
+  <p class="info">Job Last Updated ${feature.properties.logged_date}</p>
+  <p class="info">Message: ${feature.properties.message}</p>
   
   </div>`
  
   layer.bindPopup(content)
  }
 
- const tclaPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Town Centre Living Area</p>
-  <p></p>
-  <p class="info">Site Address: ${feature.properties.site_address}</p>
-  <p class="info">Site Description: ${feature.properties.site_description_2020}</p>
-  <p class="info">Site Area (Ha): ${feature.properties.site_area}</p>
-  <p class="info">Site Capacity: ${feature.properties.capacity}</p>
-  <p class="info">Delivery Years: ${feature.properties.delivery_years}</p>
-  
-  </div>`
  
-  layer.bindPopup(content)
- }
-
- const conservationPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Conservation Area</p>
-  <p></p>
-  <p class="info">Name: ${feature.properties.cons_area}</p>
-  
-  </div>`
- 
-  layer.bindPopup(content)
- }
-
- const permissionedPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Sites with planning permission for housing</p>
-  <p></p>
-  <p class="info">Site Address: ${feature.properties.site_address}</p>
-  <p class="info">Site Area (Ha): ${feature.properties.site_area}</p>
-  <p class="info">Site Net Capacity: ${feature.properties.net_capacity}</p>
-  <p class="info">Delivery Years: ${feature.properties.delivery_years}</p>
-  
-  </div>`
- 
-  layer.bindPopup(content)
- }
-
- const notassessedPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-list" aria-hidden="true"></i><p class="title">Sites that did not pass first sift of SHLAA 2020</p>
-  <p></p>
-  <p class="info">Site Address: ${feature.properties.site_address}</p>
-  <p class="info">${feature.properties.website_map_text}</p>
-
-  
-  </div>`
- 
-  layer.bindPopup(content)
- }
-
 export {
-  developablesitesPopup,
-  conservationPopup,
-  permissionedPopup,
-  notassessedPopup,
-  tclaPopup
+  streetLightingPopup 
 }
