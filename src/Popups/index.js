c9ddcd540e6391c99eeba18e2206b02054a0748a
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-01-25 12:39:28 +0000

    Popups copied in from previous map, Previous and Experimental popups commented out for further exploration

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -10,6 +10,43 @@ const streetLightingPopup = (feature, layer) => {
   layer.bindPopup(content)
 }
 
+//const streetlightFaultPopup = (feature, layer) => {
+//  const varName = getTargetUrl()
+//  let noOfDays = Math.floor(
+//    (new Date() - new Date(feature.properties.job_entry_date)) /
+//      (1000 * 3600 * 24)
+//  )
+//  let lastUpdated = Math.floor(
+//    (new Date() - new Date(feature.properties.logged_date)) / (1000 * 3600 * 24)
+//  )
+//  const defaultMessage = noOfDays
+//    ? `A fault with this street light was reported ${noOfDays} days ago`
+//    : 'A fault with this street light was reported'
+//  const showLastUpdated = lastUpdated
+//    ? `<div class="last-updated">Last updated ${lastUpdated} days ago</div>`
+//    : ''
+
+//  const content = `<div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr>
+//     <div class="item"><i class="tag fa fa-tag"></i><p class="title">Number on street light </p><p class="info">${feature.properties.central_asset_id}</p></div>
+//     <div class= "message-fault">${defaultMessage}</div>
+//     <a class="button-primary" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View reported fault</a>
+//     ${showLastUpdated}`
+//
+//  layer.bindPopup(content)
+//}
+
+//const streetlightMaintenancePopup = (feature, layer) => {
+//  const message =
+//    feature.properties.message ??
+//    'This street light is part of a maintenance programme and will be fixed without a need to report'
+//
+//  const content = `<div class="item"><span class="iconify" data-icon="fa-map-marker" data-inline="false"></span></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr>
+//     <div class="item"><i class="tag fa fa-tag"></i><p class="title">Number on street light </p><p class="info">${feature.properties.central_asset_id}</p></div>
+//     <div class= "message-maintenance">${message}</div>`
+//
+//  layer.bindPopup(content)
+//}
+
 //const streetLightingPopup = (feature, layer) => {
   
   //const content = `<div class="item"><i class="fa fa-lightbulb-o" aria-hidden="true"></i><p class="title">Street Light</p>
