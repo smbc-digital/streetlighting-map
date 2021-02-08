facf4fe972f2b33d09cf50719c779db1c0072790
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-02-08 15:49:09 +0000

    var name commented out

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,8 +1,7 @@
 import { getTargetUrl } from '../Helpers'
 
 const streetlightActivePopup = feature => {
-  const varName = getTargetUrl()
-
+  
   return `<div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr/>
   <div class="item"><i class="tag fa fa-tag"></i><p class="title">Number on street light </p><p class="info">${feature.properties.feature_id}</p></div>
   <a class="button-primary" href="report-an-issue/fault-type?assetId=${feature.properties.central_asset_id}&siteCode=${feature.properties.site_code}">Report this street light</a>`
