c9f1c1ffad8cd7f11f26d53f80852ce4e3f7b26b
Colin Lees <colinleesa7x@gmail.com>
2021-03-03 15:06:19 +0000

    fix: amend generateTargetUrl hostname lookup

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -21,11 +21,11 @@ const generateTargetUrl = () => {
     {
         return  'https://qa-dts.smbcdigital.net'
     }
-    else if (window.location.hostname.includes('staging'))
+    else if (window.location.hostname.includes('stage'))
     {
         return  'https://stage-dts.smbcdigital.net'
     }
-    else if (window.location.hostname.includes('prod'))
+    else if (window.location.hostname.includes('myaccount') || window.location.hostname.includes('forms'))
     {
         return  'https://myaccount.stockport.gov.uk'
     }
